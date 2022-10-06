0ba7fcb76e30f9ab8ec12041a842a552adaf6539
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2022-10-06 09:29:53 +0530

    Done with contracts and deployments

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            saveDeployments: true,
+            chainId: 5,
+        },
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["NftMarketplace"],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [{ version: "0.8.7" }, { version: "0.4.24" }],
+    },
+}
